19a1fb5eaa640c8081f843f531e603bf705e7fe3
jhuang20 <jhuang20@stuy.edu>
2019-05-14 10:51:11 -0400

    mod script

--- base_python/script.py
+++ base_python/script.py
@@ -1,4 +1,5 @@
 import mdl
+import sys
 from display import *
 from matrix import *
 from draw import *
@@ -27,12 +28,23 @@ def run(filename):
              [255,
               255,
               255]]
+    areflect = [0.1,
+                0.1,
+                0.1]
+    dreflect = [0.5,
+                0.5,
+                0.5]
+    sreflect = [0.5,
+                0.5,
+                0.5]
+
 
     color = [0, 0, 0]
     tmp = new_matrix()
     ident( tmp )
-
-    stack = [ [x[:] for x in tmp] ]
+    polygons=[]
+    edges=[]
+    systems = [ [x[:] for x in tmp] ]
     screen = new_screen()
     zbuffer = new_zbuffer()
     tmp = []
@@ -45,111 +57,106 @@ def run(filename):
                           'green': [0.2, 0.5, 0.5],
                           'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
-
-    print symbols
+    ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'circle', 'bezier', 'hermite', 'box', 'sphere', 'torus' ]
     for command in commands:
-        while c < len(lines):
-            line = lines[c].strip()
-            #``print ':' + line + ':'
+        step_3d=40
+        step=100
+        line=command['op']
+        #if command['constant']
+        if line in ARG_COMMANDS:
+            args =command['args']
+            #print 'args\t' + str(args)
 
-            if line in ARG_COMMANDS:
-                c+= 1
-                args = lines[c].strip().split(' ')
-                #print 'args\t' + str(args)
+        if line == 'sphere':
+            #print 'SPHERE\t' + str(args)
+            add_sphere(polygons,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step_3d)
+            matrix_mult( systems[-1], polygons )
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
+            polygons = []
 
-            if line == 'sphere':
-                #print 'SPHERE\t' + str(args)
-                add_sphere(polygons,
-                           float(args[0]), float(args[1]), float(args[2]),
-                           float(args[3]), step_3d)
-                matrix_mult( systems[-1], polygons )
-                draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
-                polygons = []
+        elif line == 'torus':
+            #print 'TORUS\t' + str(args)
+            add_torus(polygons,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), step_3d)
+            matrix_mult( systems[-1], polygons )
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            polygons = []
 
-            elif line == 'torus':
-                #print 'TORUS\t' + str(args)
-                add_torus(polygons,
-                          float(args[0]), float(args[1]), float(args[2]),
-                          float(args[3]), float(args[4]), step_3d)
-                matrix_mult( systems[-1], polygons )
-                draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
-                polygons = []
+        elif line == 'box':
+            #print 'BOX\t' + str(args)
+            add_box(polygons,
+                    float(args[0]), float(args[1]), float(args[2]),
+                    float(args[3]), float(args[4]), float(args[5]))
+            matrix_mult( systems[-1], polygons )
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            polygons = []
 
-            elif line == 'box':
-                #print 'BOX\t' + str(args)
-                add_box(polygons,
-                        float(args[0]), float(args[1]), float(args[2]),
-                        float(args[3]), float(args[4]), float(args[5]))
-                matrix_mult( systems[-1], polygons )
-                draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
-                polygons = []
+        elif line == 'circle':
+            #print 'CIRCLE\t' + str(args)
+            add_circle(edges,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step)
+            matrix_mult( systems[-1], edges )
+            draw_lines(edges, screen, zbuffer, color)
+            edges = []
 
-            elif line == 'circle':
-                #print 'CIRCLE\t' + str(args)
-                add_circle(edges,
-                           float(args[0]), float(args[1]), float(args[2]),
-                           float(args[3]), step)
-                matrix_mult( systems[-1], edges )
-                draw_lines(edges, screen, zbuffer, color)
-                edges = []
+        elif line == 'hermite' or line == 'bezier':
+            #print 'curve\t' + line + ": " + str(args)
+            add_curve(edges,
+                      float(args[0]), float(args[1]),
+                      float(args[2]), float(args[3]),
+                      float(args[4]), float(args[5]),
+                      float(args[6]), float(args[7]),
+                      step, line)
+            matrix_mult( systems[-1], edges )
+            draw_lines(edges, screen, zbuffer, color)
+            edges = []
 
-            elif line == 'hermite' or line == 'bezier':
-                #print 'curve\t' + line + ": " + str(args)
-                add_curve(edges,
-                          float(args[0]), float(args[1]),
-                          float(args[2]), float(args[3]),
-                          float(args[4]), float(args[5]),
-                          float(args[6]), float(args[7]),
-                          step, line)
-                matrix_mult( systems[-1], edges )
-                draw_lines(edges, screen, zbuffer, color)
-                edges = []
+        elif line == 'line':
+            #print 'LINE\t' + str(args)
 
-            elif line == 'line':
-                #print 'LINE\t' + str(args)
+            add_edge( edges,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), float(args[5]) )
+            matrix_mult( systems[-1], edges )
+            draw_lines(eges, screen, zbuffer, color)
+            edges = []
 
-                add_edge( edges,
-                          float(args[0]), float(args[1]), float(args[2]),
-                          float(args[3]), float(args[4]), float(args[5]) )
-                matrix_mult( systems[-1], edges )
-                draw_lines(eges, screen, zbuffer, color)
-                edges = []
+        elif line == 'scale':
+            #print 'SCALE\t' + str(args)
+            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( systems[-1], t )
+            systems[-1] = [ x[:] for x in t]
 
-            elif line == 'scale':
-                #print 'SCALE\t' + str(args)
-                t = make_scale(float(args[0]), float(args[1]), float(args[2]))
-                matrix_mult( systems[-1], t )
-                systems[-1] = [ x[:] for x in t]
+        elif line == 'move':
+            #print 'MOVE\t' + str(args)
+            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( systems[-1], t )
+            systems[-1] = [ x[:] for x in t]
 
-            elif line == 'move':
-                #print 'MOVE\t' + str(args)
-                t = make_translate(float(args[0]), float(args[1]), float(args[2]))
-                matrix_mult( systems[-1], t )
-                systems[-1] = [ x[:] for x in t]
+        elif line == 'rotate':
+            #print 'ROTATE\t' + str(args)
+            theta = float(args[1]) * (math.pi / 180)
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult( systems[-1], t )
+            systems[-1] = [ x[:] for x in t]
 
-            elif line == 'rotate':
-                #print 'ROTATE\t' + str(args)
-                theta = float(args[1]) * (math.pi / 180)
-                if args[0] == 'x':
-                    t = make_rotX(theta)
-                elif args[0] == 'y':
-                    t = make_rotY(theta)
-                else:
-                    t = make_rotZ(theta)
-                matrix_mult( systems[-1], t )
-                systems[-1] = [ x[:] for x in t]
+        elif line == 'push':
+            systems.append( [x[:] for x in systems[-1]] )
 
-            elif line == 'push':
-                systems.append( [x[:] for x in systems[-1]] )
+        elif line == 'pop':
+            systems.pop()
 
-            elif line == 'pop':
-                systems.pop()
-
-            elif line == 'display' or line == 'save':
-                if line == 'display':
-                    display(screen)
-                else:
-                    save_extension(screen, args[0])
-            c+= 1
-
-        print command
+        elif line == 'display' or line == 'save':
+            if line == 'display':
+                display(screen)
+            else:
+                save_extension(screen, args[0])
